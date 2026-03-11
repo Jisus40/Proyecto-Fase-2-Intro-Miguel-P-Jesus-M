9728c0789294b295b8c55551c341bdef2a974829
Jisus40 <184026169+Jisus40@users.noreply.github.com>
2026-03-11 01:10:36 -0400

    commit 9.1

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 let iniciarSesion = document.getElementById("iniciar");
 let mostrar = document.getElementById("iniciar-sesion");
 let formulario = document.getElementById("formulario");
@@ -33,40 +32,3 @@ entrar.onclick = function(event){
         alert("usuario o contraseña invalidos");
     }
 };
-
-=======
-let iniciarSesion = document.getElementById("iniciar");
-let mostrar = document.getElementById("iniciar-sesion");
-let formulario = document.getElementById("formulario");
-let entrar = document.getElementById("entrar");
-let usuario = document.getElementById("usuario");
-let contraseña = document.getElementById("contraseña");
-
-iniciarSesion.onclick = function(){
-	mostrar.style.display = "flex";
-	formulario.style.display = "flex";
-};
-
-entrar.onclick = function(event){
-    event.preventDefault();
-    if(usuario.value == "Cliente26" && contraseña.value == 123456789){
-        window.location.href = "Cliente/cliente.html";
-    }else if(usuario.value == "Admin" && contraseña.value == "2589vml12"){
-        alert("funciono");
-        window.location.href = "Administrador/admin.html";
-    }
-    else if(usuario.value == "Reparto12" && contraseña.value == "rep4568"){
-        alert("funciono");
-        window.location.href = "Reparto/reparto.html";
-    }else if(usuario.value == "Cocinero45" && contraseña.value == "coci1397"){
-        alert("funciono");
-        window.location.href = "Cocina/cocina.html";
-    }else if(usuario.value == "Cajero11" && contraseña.value == "Caj2956"){
-        alert("funciono");
-        window.location.href = "Cajero/cajero.html";
-    }else{
-        alert("usuario o contraseña invalidos");
-    }
-};
-
->>>>>>> 040ef4d (commit 5)
